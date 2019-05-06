f5c7acdbc5c5348cb9cd6c3bacc763e401666c84
aymericq <aquesne@gmail.com>
2019-05-06 18:16:42 +0200

    Adding new tasks via floating button + transitions when changing task status

--- src/App.vue
+++ src/App.vue
@@ -14,136 +14,182 @@
     </section>
     <section class="section">
       <div class="columns">
-        <div class="column">
+        <div class="column is-fixed">
           <div class="title">
             Not started
           </div>
-          <div v-for="card in cards_not_started" v-bind:key="card.id"
-            class="card" style="margin: 0 0 30px 0">
-            <div class="card-header">
-              <div class="card-header-title">{{ card.title }}</div>
-            </div>
-            <div class="card-content">
-              Très bieng.
-            </div>
-            <div class="card-footer">
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'wip'" class="button is-info">
-                  <span></span>
-                  <span class="icon" style="margin-right: 2px">
-                    <font-awesome-icon icon="play-circle" />
-                  </span>
-                  <span class="is-hidden-touch ">
-                    Start
-                  </span>
-                </a>
+          <transition-group name="list" tag="div">
+            <div v-for="card in cards_not_started" v-bind:key="card.id"
+              class="card list-item" style="margin: 0 0 30px 0">
+              <div class="card-header">
+                <div class="card-header-title">{{ card.title }}</div>
               </div>
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'deleted'" class="button is-danger">
-                  <span></span>
-                  <span class="icon" style="margin-right: 2px">
-                    <font-awesome-icon icon="trash" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Delete
-                  </span>
-                </a>
+              <div class="card-content">
+                {{ card.description }}
+              </div>
+              <div class="card-footer">
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'wip'" class="button is-info">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="play-circle" />
+                    </span>
+                    <span class="is-hidden-touch ">
+                      Start
+                    </span>
+                  </a>
+                </div>
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'deleted'" class="button is-danger">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="trash" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Delete
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          </transition-group>
         </div>
         <div class="is-divider-vertical"></div>
-        <div class="column">
+        <div class="column is-fixed">
           <div class="title">
             In progress
           </div>
-          <div v-for="card in cards_wip" v-bind:key="card.id"
-            class="card" style="margin: 0 0 30px 0">
-            <div class="card-header">
-              <div class="card-header-title">{{ card.title }}</div>
-            </div>
-            <div class="card-content">
-              Très bieng.
-            </div>
-            <div class="card-footer">
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'done'" class="button is-success">
-                  <span></span>
-                  <span class="icon">
-                    <font-awesome-icon icon="check-circle" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Done
-                  </span>
-                </a>
+          <transition-group name="list" tag="div">
+            <div v-for="card in cards_wip" v-bind:key="card.id"
+              class="card list-item" style="margin: 0 0 30px 0">
+              <div class="card-header">
+                <div class="card-header-title">{{ card.title }}</div>
               </div>
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'not_started'" class="button is-warning">
-                  <span></span>
-                  <span class="icon">
-                    <font-awesome-icon icon="stop-circle" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Stop
-                  </span>
-                </a>
+              <div class="card-content">
+                {{ card.description }}
               </div>
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'deleted'" class="button is-danger">
-                  <span></span>
-                  <span class="icon">
-                    <font-awesome-icon icon="trash" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Delete
-                  </span>
-                </a>
+              <div class="card-footer">
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'done'" class="button is-success">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="check-circle" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Done
+                    </span>
+                  </a>
+                </div>
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'not_started'" class="button is-warning">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="stop-circle" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Stop
+                    </span>
+                  </a>
+                </div>
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'deleted'" class="button is-danger">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="trash" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Delete
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          </transition-group>
         </div>
         <div class="is-divider-vertical"></div>
-        <div class="column">
+        <div class="column is-fixed">
           <div class="title">
             Done
           </div>
-          <div v-for="card in cards_done" v-bind:key="card.id"
-            class="card" style="margin: 0 0 30px 0">
-            <div class="card-header">
-              <div class="card-header-title">{{ card.title }}</div>
-            </div>
-            <div class="card-content">
-              Très bieng.
-            </div>
-            <div class="card-footer">
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'not_started'" class="button is-info">
-                  <span></span>
-                  <span class="icon">
-                    <font-awesome-icon icon="redo" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Reset
-                  </span>
-                </a>
+          <transition-group name="list" tag="div">
+            <div v-for="card in cards_done" v-bind:key="card.id"
+              class="card list-item" style="margin: 0 0 30px 0">
+              <div class="card-header">
+                <div class="card-header-title">{{ card.title }}</div>
               </div>
-              <div class="card-footer-item">
-                <a v-on:click="card.status = 'deleted'" class="button is-danger">
-                  <span></span>
-                  <span class="icon">
-                    <font-awesome-icon icon="trash" />
-                  </span>
-                  <span class="is-hidden-touch">
-                    Delete
-                  </span>
-                </a>
+              <div class="card-content">
+                {{ card.description }}
+              </div>
+              <div class="card-footer">
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'not_started'" class="button is-info">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="redo" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Reset
+                    </span>
+                  </a>
+                </div>
+                <div class="card-footer-item">
+                  <a v-on:click="card.status = 'deleted'" class="button is-danger">
+                    <span></span>
+                    <span class="icon" style="margin-right: 2px">
+                      <font-awesome-icon icon="trash" />
+                    </span>
+                    <span class="is-hidden-touch">
+                      Delete
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          </transition-group>
         </div>
       </div>
     </section>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <a v-on:click="showNewCardModal = true">
+      <fab class="is-primary"
+        :bg-color="bgColor"
+      ></fab>
+    </a>
+    <div v-if="showNewCardModal" class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">New task</p>
+          <button v-on:click="resetCloseNewCardModal" class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label class="label">Task title</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Title"
+                v-model="modalContent.title"
+                v-on:keyup.enter="addNewCard"
+                v-on:focusout="checkTitleValidity"
+                v-bind:class="{ 'is-danger': !isTitleValid }"
+                >
+            </div>
+            <p v-if="!isTitleValid" class="help is-danger">You must enter a title</p>
+          </div>
+          <div class="field">
+            <label class="label">Task description</label>
+            <div class="control">
+              <textarea class="textarea" placeholder="Description"
+                v-model="modalContent.description">
+              </textarea>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button v-on:click="addNewCard" class="button is-success">Add task</button>
+          <button v-on:click="resetCloseNewCardModal" class="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -155,6 +201,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash, faPlayCircle, faCheckCircle, faStopCircle, faRedo } 
   from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import fab from 'vue-fab'
 import Vue from 'vue'
 
 library.add(faTrash)
@@ -175,25 +222,19 @@ export default {
       cards: [
         {
           id: 0,
-          title: "Hello",
+          title: "Sample take",
+          description: "This is a sample description for a sample task.",
           status: 'not_started'
         },
-        {
-          id: 1,
-          title: "Helloooo",
-          status: "not_started"
-        },
-        {
-          id: 2,
-          title: "Chibre.",
-          status: "done"
-        },
-        {
-          id: 3,
-          title: "Chibron.",
-          status: "wip"
-        }
-      ]
+      ],
+      lastCardIndex: 0,
+      bgColor: "hsl(171, 100%, 41%)",
+      showNewCardModal: false,
+      modalContent: {
+        title: null,
+        description: null
+      },
+      isTitleValid: true,
     }
   },
   computed: {
@@ -207,8 +248,35 @@ export default {
       return this.cards.filter(item => (item.status == "done"))
     }
   },
+  methods: {
+    addNewCard: function() {
+      this.checkTitleValidity();
+      if(this.isTitleValid) {
+        this.lastCardIndex += 1;
+        this.cards.push({
+          id: this.lastCardIndex,
+          title: this.modalContent.title,
+          description: this.modalContent.description,
+          status: "not_started"
+        });
+        this.resetCloseNewCardModal();
+      }
+    },
+    checkTitleValidity: function() {
+      this.isTitleValid = !!this.modalContent.title;
+    },
+    resetCloseNewCardModal: function() {
+      this.modalContent = {
+        title: null,
+        description: null
+      };
+      this.showNewCardModal = false;
+      this.showTitleEmptyWarning = false;
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    fab
   }
 }
 </script>
